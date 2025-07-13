5cf477e45e4c3945bd0642856cb50fff434cbcff
Max Roald Eckardt <mr.eckardt@gmail.com>
2025-07-14 01:03:13 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ['js', 'json', 'vue', 'mjs'],
   transform: {
     '^.+\\.vue$': '@vue/vue3-jest',
-    '^.+\\.js$': 'babel-jest'
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.mjs$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@?mermaid|@mermaid-js|d3|d3-.*|dagre|graphlib|internmap|delaunator|robust-predicates|khroma|dayjs|uuid|micromark|mdast|unist|hast|rehype|remark|vfile|lodash-es|.*\\.mjs$))'
+  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
